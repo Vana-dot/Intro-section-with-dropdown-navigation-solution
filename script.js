ece57cec844eedca59337f0504fc3651ae579e13
Geovana Venancio <geovanavenancio5556@gmail.com>
2022-07-03 15:53:49 -0300

    main

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var img = document.querySelector("#arrow");
 function trocas(){
     if(container.style.display === 'none') {
         container.style.display = 'flex';
-        img.src="./images/icon-arrow-up.svg";
+        img.src="./assets/images/icon-arrow-up.svg";
     } else {
         container.style.display = 'none';
-        img.src="./images/icon-arrow-down.svg";
+        img.src="./assets/images/icon-arrow-down.svg";
     }
    
     
@@ -18,10 +18,10 @@ var imgtwo = document.querySelector("#arrowtwo");
 function trocastwo(){
     if(containertwo.style.display === 'none') {
         containertwo.style.display = 'flex';
-        imgtwo.src="./images/icon-arrow-up.svg";
+        imgtwo.src="./assets/images/icon-arrow-up.svg";
     } else {
         containertwo.style.display = 'none';
-        imgtwo.src="./images/icon-arrow-down.svg";
+        imgtwo.src="./assets/images/icon-arrow-down.svg";
     }
    
     
